ab62288d3de882d43e6c8215ba09b72eb34bf652
caiyf <778197259@qq.com>
2019-04-10 17:12:50 +0800

    init project

---
+++ src/main/java/com/caiyf/appliance/repair/bs/ApplianceRepairBsApplication.java
@@ -0,0 +1,13 @@
+package com.caiyf.appliance.repair.bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplianceRepairBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplianceRepairBsApplication.class, args);
+	}
+
+}
