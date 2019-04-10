021b7501dd3baa8d9e85d9722d8fb5b51240b1ef
caiyf <778197259@qq.com>
2019-04-10 18:26:50 +0800

    init result

---
+++ src/main/java/com/caiyf/appliance/repair/bs/model/result/CodeEnum.java
@@ -0,0 +1,29 @@
+package com.caiyf.appliance.repair.bs.model.result;
+
+/**
+ * @desc 状态码
+ * @date 2019/04/10
+ * @author caiyf
+ */
+public enum CodeEnum {
+
+    /** 通用状态码 */
+    SUCCESS(1000, "请求成功"),
+    PARAMETER_VALID_ERROR(1001, "参数异常");
+
+    private Integer code;
+    private String msg;
+
+    private CodeEnum(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+    public String getMsg() {
+        return msg;
+    }
+
+}
