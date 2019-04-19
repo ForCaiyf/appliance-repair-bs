58d5fb36cfa675f0417525477fb45f5353f4156d
caiyf <778197259@qq.com>
2019-04-19 21:51:17 +0800

    init db2

--- src/main/resources/mybatis/script/2019041417_init_tables.sql
+++ src/main/resources/mybatis/script/2019041417_init_tables.sql
@@ -1,21 +1,8 @@
-CREATE TABLE `staff` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `num` int(6) NOT NULL DEFAULT 0 COMMENT '员工id',
-  `password` varchar(32) NOT NULL DEFAULT '' COMMENT '员工密码',
-  `type` int(1) NOT NULL DEFAULT 0 COMMENT '员工类别（1-初级人员、2-中级人员、3-高级人员）',
-  `level` int(1) NOT NULL DEFAULT 0 COMMENT '员工级别（1-维修员、2-业务员、3-管理员）',
-  `info_id` int(11) NOT NULL DEFAULT 0 COMMENT '员工信息表id',
-  `agent_id` int(11) NOT NULL DEFAULT 0 COMMENT '经办人id',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `is_deleted` int(1) NOT NULL DEFAULT 0 COMMENT '是否删除（0-未删除、1-删除）',
-  PRIMARY KEY `id_index` (`id`) COMMENT 'id主键'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '员工表';
-
 CREATE TABLE `staff_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `name` varchar(8) NOT NULL DEFAULT '' COMMENT '姓名',
   `age` int(3) NOT NULL DEFAULT 0 COMMENT '年龄',
-  `sex` int(1) NOT NULL DEFAULT 0 COMMENT '性别index',
+  `sex_id` int(1) NOT NULL DEFAULT 0 COMMENT '性别id',
   `id_card_encrypt` varchar(64) NOT NULL DEFAULT '' COMMENT '身份证',
   `phone_encrypr` varchar(32) NOT NULL DEFAULT '' COMMENT '手机号',
   `email` varchar(32) NOT NULL DEFAULT '' COMMENT '邮箱',
@@ -35,3 +22,46 @@ CREATE TABLE `staff_info` (
   PRIMARY KEY `id_index` (`id`) COMMENT 'id主键'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '员工信息表';
 
+INSERT INTO `staff_info` (`name`, `age`, `sex_id`, `id_card_encrypt`, `phone_encrypr`, `email`, `school`, `education_id`, `nation_id`, `marriage_id`, `experience`, `image_url`, `address`)
+    VALUES ('caiyf', 23, 1, 'KI705sKPp++h4aXBpyULpyG54bryiPoh', 'K7iLGXmIxuBTCi68urNP1A==', 'caiyf_wx@163.com', '东北林业大学', '6', '1', '1', '。。。', 'D:\\Program Files\\IDEAwork\\images\\caiyf.jpg', '黑龙江|哈尔滨|香坊|东北林业大学');
+
+CREATE TABLE `staff_type` (
+  `id` int(2) NOT NULL COMMENT '类型id',
+  `type` varchar(8) NOT NULL DEFAULT '' COMMENT '类型名',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `is_deleted` int(1) NOT NULL DEFAULT 0 COMMENT '是否删除（0-未删除、1-删除）',
+  PRIMARY KEY `id_index` (`id`) COMMENT 'id主键'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '员工类型表';
+
+INSERT INTO `staff_type` (`id`, `type`) VALUES (1, '维修员');
+INSERT INTO `staff_type` (`id`, `type`) VALUES (2, '业务员');
+INSERT INTO `staff_type` (`id`, `type`) VALUES (3, '管理员');
+
+CREATE TABLE `staff_level` (
+  `id` int(2) NOT NULL COMMENT '级别id',
+  `level` varchar(8) NOT NULL DEFAULT '' COMMENT '级别名',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `is_deleted` int(1) NOT NULL DEFAULT 0 COMMENT '是否删除（0-未删除、1-删除）',
+  PRIMARY KEY `id_index` (`id`) COMMENT 'id主键'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '员工级别表';
+
+INSERT INTO `staff_level` (`id`, `level`) VALUES (1, '初级');
+INSERT INTO `staff_level` (`id`, `level`) VALUES (2, '中级');
+INSERT INTO `staff_level` (`id`, `level`) VALUES (3, '高级');
+
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `num` int(6) NOT NULL DEFAULT 0 COMMENT '员工id',
+  `name` varchar(8) NOT NULL DEFAULT '' COMMENT '员工姓名',
+  `password` varchar(32) NOT NULL DEFAULT '' COMMENT '员工密码',
+  `type` int(1) NOT NULL DEFAULT 0 COMMENT '员工类别',
+  `level` int(1) NOT NULL DEFAULT 0 COMMENT '员工级别',
+  `info_id` int(11) NOT NULL DEFAULT 0 COMMENT '员工信息表id',
+  `agent_id` int(11) NOT NULL DEFAULT 0 COMMENT '经办人id',
+  `info_filled` int(1) NOT NULL DEFAULT 1 COMMENT '个人信息是否完善（0-完善、1-不完善）',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `is_deleted` int(1) NOT NULL DEFAULT 0 COMMENT '是否删除（0-未删除、1-删除）',
+  PRIMARY KEY `id_index` (`id`) COMMENT 'id主键'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '员工表';
+
+INSERT INTO `staff` (`num`, `name`, `password`, `type`, `level`, `info_id`) VALUE (888888, 'caiyf', 'cyf1996', 3, 3, 1);
